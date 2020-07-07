57012adad6b63672a93cb0dc691b4e916f606025
cabravoac <49000086+cabravoac@users.noreply.github.com>
2020-07-06 20:15:56 -0500

    lun6

--- js/scroll_javascript.js
+++ js/scroll_javascript.js
@@ -2,6 +2,8 @@ function Scroll() {
 	var element = document.querySelector(".header");
 	var type = document.querySelector(".p-scroll");
 	var type2 = document.querySelector(".p-scroll2");
+	var type3 = document.querySelector(".p-scroll3");
+	var type4 = document.querySelector(".p-scroll4");
 	var box = document.querySelector(".searchbox");
 	var placeholder = document.querySelector(".placeholder");
 
@@ -11,6 +13,8 @@ function Scroll() {
 		  element.classList.add("header-scrolled");
 		  type.classList.add("p-scrolled");
 		  type2.classList.add("p-scrolled2");
+		  type3.classList.add("p-scrolled3");
+		  type4.classList.add("p-scrolled4");
 		  box.classList.add("searchbox-scrolled");
 		  placeholder.classList.add("placeholder-scrolled")
 	/*
@@ -26,6 +30,8 @@ function Scroll() {
 			element.classList.remove("header-scrolled");
 			type.classList.remove("p-scrolled");
 			type2.classList.remove("p-scrolled2");
+			type3.classList.remove("p-scrolled3");
+			type4.classList.remove("p-scrolled4");
 			box.classList.remove("searchbox-scrolled");
 			placeholder.classList.remove("placeholder-scrolled")
 
